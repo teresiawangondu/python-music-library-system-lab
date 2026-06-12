eb33fcec7b22164cf60c07c87aaf89675e01a5f3
teresiawangondu <arwatess@gmail.com>
2026-06-12 14:57:55 +0300

    fix bug

--- lib/song.py
+++ lib/song.py
@@ -1,63 +1,24 @@
 class Song:
-    # Class attributes
-    total_songs = 0
-    all_artists = []
-    all_genres = []
-    genre_count = {}
-    artist_count = {}
+    count = 0
+    genres = {}
+    artists = {}
 
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
         self.genre = genre
 
-        # Update class attributes
-        Song.total_songs += 1
+        # Total number of Song instances
+        Song.count += 1
 
-        if artist not in Song.all_artists:
-            Song.all_artists.append(artist)
+        # Count songs by genre
+        if genre in Song.genres:
+            Song.genres[genre] += 1
+        else:
+            Song.genres[genre] = 1
 
-        if genre not in Song.all_genres:
-            Song.all_genres.append(genre)
-
-        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
-        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
-
-    def play(self):
-        return f"Playing '{self.name}' by {self.artist}"
-
-    @classmethod
-    def get_total_songs(cls):
-        return cls.total_songs
-
-    @classmethod
-    def get_all_artists(cls):
-        return cls.all_artists
-
-    @classmethod
-    def get_all_genres(cls):
-        return cls.all_genres
-
-    @classmethod
-    def get_genre_count(cls, genre):
-        return cls.genre_count.get(genre, 0)
-
-    @classmethod
-    def get_artist_count(cls, artist):
-        return cls.artist_count.get(artist, 0)
-
-
-# Example usage / testing
-if __name__ == "__main__":
-    song1 = Song("Blinding Lights", "The Weeknd", "Pop")
-    song2 = Song("Levitating", "Dua Lipa", "Pop")
-    song3 = Song("Bad Guy", "Billie Eilish", "Pop")
-    song4 = Song("Rolling in the Deep", "Adele", "Soul")
-
-    print(song1.play())
-    print("Total songs:", Song.get_total_songs())
-    print("All artists:", Song.get_all_artists())
-    print("All genres:", Song.get_all_genres())
-    print("Pop genre count:", Song.get_genre_count("Pop"))
-    print("Dua Lipa song count:", Song.get_artist_count("Dua Lipa"))
-    pass
+        # Count songs by artist
+        if artist in Song.artists:
+            Song.artists[artist] += 1
+        else:
+            Song.artists[artist] = 1
